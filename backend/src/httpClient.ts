2242cd6fa6c6bd20b28252b1e26acfbc6039cbf2
musahabibulloh <musahabibullah3@gmail.com>
2026-09-19 02:52:00 +0700

    Remove IP hack that breaks SSL on Vercel

--- backend/src/httpClient.ts
+++ backend/src/httpClient.ts
@@ -5,12 +5,7 @@ export const httpClient = {
         try {
             let targetUrl = url;
             const headers: any = {};
-            if (url.includes('lk21official.cc')) {
-                const parsed = new URL(url);
-                headers['Host'] = parsed.hostname;
-                parsed.hostname = '104.21.59.235';
-                targetUrl = parsed.toString();
-            }
+
             const html = await cloudscraper({ method: 'GET', url: targetUrl, headers });
             return { data: html };
         } catch (error) {
